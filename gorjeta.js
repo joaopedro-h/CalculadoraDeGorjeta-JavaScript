6a2cf6ffb3f93ecdf1b428357a575980e1803c79
João Pedro <joao.hermenegildocosta@gmail.com>
2026-03-16 20:56:16 -0300

    Adiciona formatação de valor em BRL e cálculo de gorjeta por pessoa

--- gorjeta.js
+++ gorjeta.js
@@ -1,17 +1,24 @@
+function formatacaoValor(valor) { /* Função criada para adicionar "R$" antes do valor e deixar apenas duas casas decimais. */
+    valor = valor.toFixed(2);
+    return "R$" + valor
+}
+
 function gorjeta() {
     
     let conta = document.getElementById("suaConta").value;
     let porcentagemGorjeta = document.getElementById("campoGorjeta").value;
-    let divisaoPessoas =   document.getElementById("divisaoPessoas").value;
-
+    let divisaoPessoas = document.getElementById("divisaoPessoas").value;
+    
     document.getElementById("porcentagemGorjeta").textContent = porcentagemGorjeta + "%";
     document.getElementById("quantidadePessoas").textContent = divisaoPessoas;
 
     let valorGorjeta = conta * (porcentagemGorjeta / 100);
     let totalComGorjeta = Number(conta) + valorGorjeta;
     let cadaPessoa = totalComGorjeta / divisaoPessoas;
-
-    document.getElementById("valorGorjeta").textContent = valorGorjeta;
-    document.getElementById("totalComGorjeta").textContent = totalComGorjeta;
-    document.getElementById("cadaPessoa").textContent = cadaPessoa;
+    let gorjetaPessoa = valorGorjeta / divisaoPessoas;
+    
+    document.getElementById("valorGorjeta").textContent = formatacaoValor(valorGorjeta);
+    document.getElementById("totalComGorjeta").textContent = formatacaoValor(totalComGorjeta);
+    document.getElementById("cadaPessoa").textContent = formatacaoValor(cadaPessoa);
+    document.getElementById("gorjetaPessoa").textContent = formatacaoValor(gorjetaPessoa);
 }
